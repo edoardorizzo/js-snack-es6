8902250e3f03bff6c79ead2c7192c5b885379a66
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-23 12:39:23 +0100

    add first letter uppercase

--- snack2/main.js
+++ snack2/main.js
@@ -3,3 +3,13 @@ A partire da un array di stringhe, crea un secondo array formattando le stringhe
 
 Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’]
 */
+
+const stringArray = ['casa', 'cibo' , 'frigo', 'cucina'];
+//console.log(stringArray);
+
+stringArray.forEach((word => {
+    const toUppercase = word.charAt(0).toUpperCase() + word.slice(1);
+    console.log(toUppercase);
+    return toUppercase
+}))
+console.log(stringArray);
